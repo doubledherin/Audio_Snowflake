a9e0e98e7b3a1899b9d2faa2d6af207993d2ef22
Wendy Dherin <wendydherin@gmail.com>
2014-11-04 12:25:15 -0800

    Adding .gitignore and get_song_data.py (the script that gets song data from the Echonest API).

---
+++ get_song_data.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import os
+import json, requests
+
+api_key = os.environ.get("ECHO_NEST_API_KEY")
+
+# calls Echonest API with artist name and song title (strings);
+# returns a dictionary of song data
+def get_song_data(artist, title):
+	print api_key
+	r = requests.get("http://developer.echonest.com/api/v4/song/search?" + api_key + "&format=json&results=1&artist=" + artist + "&title=" + title + "&bucket=audio_summary")
+
+	status_code = r.status_code
+	results = json.loads(r.content)
+
+	song_data = {}
+
+	# get general song info
+	song_general = results["response"]["songs"][0]
+	
+	for key, value in song_general.iteritems():
+		# skip audio_summary, which we're getting in the next for loop
+		if key == "audio_summary":
+			continue
+		song_data[key] = value
+
+	# get detailed song info	
+	audio_summary = song_general["audio_summary"]
+
+	for key, value in audio_summary.iteritems():
+		song_data[key] = value
+
+	for key, value in song_data.iteritems():
+		print key, value
+
+
+	return song_data
