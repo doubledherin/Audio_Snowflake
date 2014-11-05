f672f87cfaeda95b19d29bb860ea2a235bc8082f
Wendy Dherin <wendydherin@gmail.com>
2014-11-04 18:50:21 -0800

    Added collapse_sections function, which combines sections with the same key and mode.

--- get_song_data.py
+++ get_song_data.py
@@ -41,7 +41,7 @@ def get_song_data(artist, title):
 	return song_data
 
 # get info on song sections and add to song_data dictionary
-def process_sections(artist, title):
+def add_sections(artist, title):
 
 	song_data = get_song_data(artist, title)
 	
@@ -51,21 +51,36 @@ def process_sections(artist, title):
 	status_code = r1.status_code
 	r1 = json.loads(r1.content)
 
+	#TO DO: Build in handler in the case that sections data is limited.
 	sections = r1["sections"]
+	song_data["sections"] = sections
+
 
 	song_data["num_sections"] = len(sections)
 
-	section_dict = {}
-	for i in range(len(sections)):
-		section_dict["section%d" % i] = sections[i]
-
-	song_data["section_dict"] = section_dict
-
 	return song_data
+
+# takes and artist/title combo, gets the song data, gets the section data
+# then collapses the sections into similar groups (those with the same key and mode)
+def collapse_sections(artist, title):
+	song_data = add_sections(artist, title)
+	sections = song_data["sections"]
+
+	collapsed = {}
+
+	for i in range(len(sections)):
+		key = sections[i]["key"]
+		mode = sections[i]["mode"]
+		collapsed[(key, mode)] = collapsed.setdefault((key, mode), [])
+		collapsed[(key, mode)].append(sections[i])
+	
+	for key, value in collapsed.iteritems():
+		print "Key: %r, Value: %r\n" % (key, value)
+	print song_data["num_sections"]
 
 def main():
 	script, artist, title = argv
-	print process_sections(artist, title)
+	print collapse_sections(artist, title)
 
 if __name__ == "__main__":
 	main()
