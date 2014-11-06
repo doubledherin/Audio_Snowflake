ab2cccfa08750581e5fd04075416535627f78532
Wendy Dherin <wendydherin@gmail.com>
2014-11-06 13:27:38 -0800

    Adding static and templates directories.

---
+++ app/static/js/init.js
@@ -0,0 +1,8 @@
+window.onload = function()
+{
+  var canvas = document.getElementsByTagName("canvas");
+  for ( var i = 0; i < canvas.length; i++ )
+  {
+    Processing( canvas[i], canvas[i].previousSibling.textContent );
+  }
+};
