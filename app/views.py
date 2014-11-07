2d27cbb8a4609e52a138acc8c3f5991391fc3a60
Wendy Dherin <wendydherin@gmail.com>
2014-11-07 15:03:03 -0800

    Added scaffolding and changed parameter name from 'track_id' to 'spotify_track_id'

--- app/views.py
+++ app/views.py
@@ -9,8 +9,14 @@ app.secret_key = "ADFLKASDJF"
 
 @app.route("/")
 def index():
-    # TO DO add code to randomly generate an image
-    return render_template("index.html", track_id=track_id)
+    
+    # TO DO: randomly get a spotify_track_id from database
+
+
+    # Right now, it always starts with Southern Belle (Elliott Smith)
+    spotify_track_id = "0um6CMHbyaJCr0aHL2OdRU"
+
+    return render_template("index.html", spotify_track_id=spotify_track_id)
 
 @app.route("/new_song")
 def get_new_song():
@@ -21,19 +27,20 @@ def get_new_song():
     # check to see if song is in database. 
     song = m.session.query(m.Track).filter_by(title=title).filter_by(artist_name=artist_name).first()
 
-    # if it is, return information for it and render it on screen
+    # TO DO: if it is, return information for it and render it on screen
     # (placeholder below)
-    song_data = None
+    if song:
+        song_data = None
 
-    # if it is not, call Echonest
+    # TO DO: if it is not, call Echonest
+    else:
+        pass
+
+    # TO DO: then call Spotify for the music player
 
 
-    # then call Spotify for the music player
-    spotify_track_id = "0um6CMHbyaJCr0aHL2OdRU"
 
-
-    # then store Echonest and Spotify data in database
-
+    # TO DO: then store Echonest and Spotify data in database
 
     return render_template("index.html", spotify_track_id=spotify_track_id)
 
