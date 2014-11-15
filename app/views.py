ac243af834f5c6936e00ad091c242da088324395
Wendy Dherin <wendydherin@gmail.com>
2014-11-14 17:58:50 -0800

    Cleaned up comments and print debuggers in views.py.

--- app/views.py
+++ app/views.py
@@ -1,14 +1,10 @@
 import json, random
-
+import model as m 
 from flask import Flask, jsonify, render_template, redirect, request, flash, url_for
 from flask import session as browser_session
-import model as m 
 from model import db_session
-
 from sqlalchemy import desc
-
 from api_calls import algorithm
-
 from seed import add_to_db
 
 app = Flask(__name__)
@@ -22,9 +18,6 @@ def index():
     rand = random.randrange(0, db_session.query(m.Track).count()) 
     track = db_session.query(m.Track)[rand]
 
-    # spotify_track_uri = track.spotify_track_uri
-    # patterns = track.patterns
-
     return render_template("index.html", track=track)
 
 @app.route("/get_patterns")
@@ -32,29 +25,19 @@ def get_pattern():
 
     title = request.args.get("title")
     artist_name = request.args.get("artist_name")
-    print 000, title, artist_name
     
     # check to see if song is in database. 
     track = m.search(artist_name, title)
 
     if track:
-        # patterns = song.patterns
-        # spotify_track_uri = song.spotify_track_uri
-        print "I FOUND ME  A TRACKKKKKK"
         return render_template("index.html", track=track)
 
     # If it is not in the db yet, call Echonest to get it
     else:
         song_data = algorithm(artist_name, title)
-
         add_to_db(db_session, song_data)
-        
         track = m.search(artist_name, title)
         
-        # # keep track uri for web player
-        # spotify_track_uri = song_data["spotify_track_uri"]
-        # patterns = song_data["patterns"]
-
         return render_template("index.html", track=track) 
 
 
