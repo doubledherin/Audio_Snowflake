8ffe2b050c262eb117e7eaa3c4918c641c70514f
Wendy Dherin <wendydherin@gmail.com>
2014-11-26 14:15:00 -0800

    Removed dead code from add_to_db.py.

--- app/add_to_db.py
+++ app/add_to_db.py
@@ -19,24 +19,18 @@ def add_song_to_db(session, song_data):
     track.title = song_data["title"]
     track.tempo = song_data["tempo"]
     track.energy = song_data["energy"]
-    # track.liveness = song_data["liveness"]
-    # track.speechiness = song_data["speechiness"]
     track.artist_name = song_data["artist_name"]
     track.mode = song_data["mode"]
-    # track.acousticness = song_data["acousticness"]
-    # track.danceability = song_data["danceability"]
     track.time_signature = song_data["time_signature"]
     track.duration = song_data["duration"]
     track.loudness = song_data["loudness"]
     track.artist_id = song_data["artist_id"]
     track.valence = song_data["valence"]    
     track.audio_md5 = song_data["audio_md5"]
-    # track.instrumentalness = song_data["instrumentalness"]
     track.spotify_track_uri = song_data["spotify_track_uri"]
     track.patterns = patterns_json
     track.sections = sections_json
     track.rotation_duration = song_data["rotation_duration"]
-    print "track sections", track.sections
 
     db_session.add(track)
     db_session.commit()
