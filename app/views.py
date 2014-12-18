1a8d7b5bbb13c8497b2fe2c14af2316ab99cbd19
Wendy Dherin <wendydherin@gmail.com>
2014-12-17 16:21:58 -0800

    Use Heroku port & open interface

--- app/views.py
+++ app/views.py
@@ -136,7 +136,7 @@ def gallery_page():
 
 if __name__ == "__main__":
 
-    
-    app.run(debug=True)
+    PORT = int(os.environ.get("PORT", 5000))
+    app.run(app.run(debug=True, host="0.0.0.0", port=PORT))
 
 
