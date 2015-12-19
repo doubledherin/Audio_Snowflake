605d8b1c810441059239835bab3953aa5a12af2f
Wendy Dherin <wendydherin@gmail.com>
2015-12-19 09:49:18 -0800

    Reorganize and add to install script.

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 !#/bin/bash
 
-sudo apt-get install python-virtualenv
-sudo apt-get install postgresql-server-dev-9.3
-sudo apt-get install postgresql
+sudo apt-get update
+sudo apt-get install python-dev -y
+sudo apt-get install python-virtualenv -y
+sudo apt-get install postgresql-server-dev-9.3 -y
+sudo apt-get install postgresql -y
 virtualenv venv
 source venv/bin/activate
-pip install -r requirements.txt
+pip install Flask==0.10.1
+pip install Flask-WTF==0.10.3
+pip install Jinja2==2.7.3
+pip install SQLAlchemy==0.9.8
+pip install WTForms==2.0.1
+pip install requests==2.4.3
+pip install spotipy==2.1.0
+pip install psycopg2==2.5.4
