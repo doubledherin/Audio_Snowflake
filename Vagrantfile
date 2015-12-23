6ff5c8245572b89cd69b75821af48f3cffa3d8d2
Wendy Dherin <wendydherin@gmail.com>
2015-12-22 22:37:23 -0800

    Update install script and change forwarding port to 5000.

--- Vagrantfile
+++ Vagrantfile
@@ -4,7 +4,7 @@
 Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/trusty64"
   config.vm.box_check_update = false
-  config.vm.network "forwarded_port", guest: 8888, host: 8888
+  config.vm.network "forwarded_port", guest: 5000, host: 5000
   config.vm.network "private_network", ip: "192.168.33.10"
   config.vm.synced_folder ".", "/vagrant"
   config.vm.provider "virtualbox" do |vb|
--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
-!#/bin/bash
+#!/usr/bin/env bash
 
 sudo apt-get update
 sudo apt-get install python-dev -y
 sudo apt-get install python-virtualenv -y
 sudo apt-get install postgresql-server-dev-9.3 -y
 sudo apt-get install postgresql -y
+sudo apt-get install postgresql-contrib -y
 virtualenv venv
 source venv/bin/activate
 pip install Flask==0.10.1
@@ -15,3 +16,6 @@ pip install WTForms==2.0.1
 pip install requests==2.4.3
 pip install spotipy==2.1.0
 pip install psycopg2==2.5.4
+
+sudo -u postgres createuser -P -s -e vagrant
+createdb audiosnowflake
